5aff53bb4d90f59e2ca3589cd55af76fed8fe83b
anderson <anderson.jose.vasquez.04.95@gmail.com>
2023-02-16 11:27:05 -0400

    jjdj

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,20 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppConfig } from './config/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductosModule } from './modules/productos/productos.module';
-import { VentasModule } from './modules/ventas/ventas.module';
-import { GastosModule } from './modules/gatos/gastos.module';
+// import { VentasModule } from './modules/ventas/ventas.module';
+// import { GastosModule } from './modules/gatos/gastos.module';
 import { UserModule } from './modules/user/user.module';
+import { MovementsModule } from './modules/movements/movements.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(AppConfig.dataBaseUrl),
     AuthModule,
     ProductosModule,
-    VentasModule,
-    GastosModule,
+    // VentasModule,
+    // GastosModule,
     UserModule,
+    MovementsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
